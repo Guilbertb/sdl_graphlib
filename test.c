7c2d6dacbcb1edbcabc27f6d32a97301f2a8cec8
workboot <guilbert@workboot.fr>
2022-06-07 11:48:13 +0200

    Add files via upload

--- test.c
+++ test.c
@@ -13,6 +13,10 @@
 
 int main (int argc , char ** argv)
 {
+if (argc==1) 	{
+		printf ("Programme : %s \n",argv[0]);
+		}
+
 initSDL(800,600,"Test EASY");
 ligne(10,10,150,160,0xFF0000);
 disque(350,160,100,0x00FF00);
